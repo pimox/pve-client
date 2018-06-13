4cedebf640a4b4862a98eb492ddb2f3c56222677
René Jochum <r.jochum@proxmox.com>
2018-06-13 10:44:59 +0200

    Add spice command

--- PVE/APIClient/Commands/GuestStatus.pm
+++ PVE/APIClient/Commands/GuestStatus.pm
@@ -8,6 +8,8 @@ use PVE::APIClient::Config;
 
 use PVE::JSONSchema qw(get_standard_option);
 
+use File::Temp qw(tempfile);
+
 use PVE::CLIHandler;
 
 use base qw(PVE::CLIHandler);
@@ -69,6 +71,48 @@ __PACKAGE__->register_method ({
 	my $vmid = PVE::Tools::extract_param($param, 'vmid');
 
 	$guest_status_command->($remote, $vmid, 'stop', $param);
+
+	return undef;
+    }});
+
+__PACKAGE__->register_method ({
+    name => 'spice',
+    path => 'spice',
+    method => 'POST',
+    description => "Run the spice client for a guest (VM/Container)",
+    parameters => {
+	additionalProperties => 0,
+	properties => {
+	    remote => get_standard_option('pveclient-remote-name'),
+	    vmid => get_standard_option('pve-vmid'),
+	},
+    },
+    returns => { type => 'null'},
+    code => sub {
+	my ($param) = @_;
+
+	my $remote = PVE::Tools::extract_param($param, 'remote');
+	my $vmid = PVE::Tools::extract_param($param, 'vmid');
+
+	my $config = PVE::APIClient::Config->load();
+	my $conn = PVE::APIClient::Config->remote_conn($config, $remote);
+
+	my $resource = PVE::APIClient::Helpers::get_vmid_resource($conn, $vmid);
+
+	my $res = $conn->post("api2/json/nodes/$resource->{node}/$resource->{type}/$resource->{vmid}/spiceproxy", {});
+
+	my $vvsetup = "[virt-viewer]\n";
+	foreach my $k (keys %$res) {
+	    $vvsetup .= "$k=$res->{$k}\n";
+	}
+
+	my ($fh, $filename) = tempfile( "tempXXXXX", SUFFIX => '.vv', TMPDIR => 1);
+	syswrite($fh, $vvsetup);
+
+	system("nohup remote-viewer $filename 1>/dev/null 2>&1 &");
+	if ($? != 0) {
+	    print "failed to execute remote-viewer: $!\n";
+	}
 
 	return undef;
     }});
