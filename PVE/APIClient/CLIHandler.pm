826591e032a3d2161abf21b5007f116431ecd22a
Dietmar Maurer <dietmar@proxmox.com>
2018-06-22 09:47:25 +0200

    update files from pve-common

--- PVE/APIClient/CLIHandler.pm
+++ PVE/APIClient/CLIHandler.pm
@@ -2,6 +2,7 @@ package PVE::APIClient::CLIHandler;
 
 use strict;
 use warnings;
+use JSON;
 
 use PVE::APIClient::SafeSyslog;
 use PVE::APIClient::Exception qw(raise raise_param_exc);
@@ -428,6 +429,18 @@ my $print_bash_completion = sub {
     &$print_result(@option_list);
 };
 
+sub data_to_text {
+    my ($data) = @_;
+
+    return undef if !defined($data);
+
+    if (my $class = ref($data)) {
+	return to_json($data, { utf8 => 1, canonical => 1 });
+    } else {
+	return "$data";
+    }
+}
+
 # prints a formatted table with a title row.
 # $formatopts is an array of hashes, with the following keys:
 # 'key' - key of $data element to print
@@ -436,29 +449,33 @@ my $print_bash_completion = sub {
 #            the last column will never be cutoff
 # 'default' - optional default value for the column
 # formatopts element order defines column order (left to right)
+# sorts the output according to the leftmost column not containing any undef
 sub print_text_table {
     my ($formatopts, $data) = @_;
 
-    my ($formatstring, @keys, @titles, %cutoffs, %defaults);
+    my ($formatstring, @keys, @titles, %cutoffs, %defaults, $sort_key);
     my $last_col = $formatopts->[$#{$formatopts}];
 
     foreach my $col ( @$formatopts ) {
-	my ($key, $title, $cutoff, $default) = @$col{qw(key title cutoff default)};
+	my ($key, $title, $cutoff) = @$col{qw(key title cutoff)};
 	$title //= $key;
 
 	push @keys, $key;
 	push @titles, $title;
-	$defaults{$key} = $default;
+	$defaults{$key} = $col->{default} // '';
 
 	# calculate maximal print width and cutoff
 	my $titlelen = length($title);
 
 	my $longest = $titlelen;
+	my $sortable = 1;
 	foreach my $entry (@$data) {
-	    my $len = length($entry->{$key}) // 0;
+	    my $len = length(data_to_text($entry->{$key})) // 0;
 	    $longest = $len if $len > $longest;
+	    $sortable = 0 if !defined($entry->{$key});
 	}
 
+	$sort_key //= $key if $sortable;
 	$cutoff = (defined($cutoff) && $cutoff < $longest) ? $cutoff : $longest;
 	$cutoffs{$key} = $cutoff;
 
@@ -472,32 +489,40 @@ sub print_text_table {
 
     printf $formatstring, @titles;
 
-    foreach my $entry (sort { $a->{$keys[0]} cmp $b->{$keys[0]} } @$data) {
-        printf $formatstring, map { substr(($entry->{$_} // $defaults{$_}), 0 , $cutoffs{$_}) } @keys;
+    if (defined($sort_key)){
+	@$data = sort { $a->{$sort_key} cmp $b->{$sort_key} } @$data;
+    }
+    foreach my $entry (@$data) {
+        printf $formatstring, map { substr((data_to_text($entry->{$_}) // $defaults{$_}), 0 , $cutoffs{$_}) } @keys;
     }
 }
 
-sub print_entry {
-    my $entry = shift;
-
-    #TODO: handle objects/hashes as well
-    foreach my $item (sort keys %$entry) {
-	if (ref($entry->{$item}) eq 'ARRAY') {
-	    printf "%s: [ %s ]\n", $item, join(", ", @{$entry->{$item}});
-	} else {
-	    printf "%s: %s\n", $item, $entry->{$item};
-	}
-    }
-}
-
-# prints the result of an API GET call returning an array
-# and to have the results key of the API call defined.
+# prints the result of an API GET call returning an array as a table.
+# takes formatting information from the results property of the call
+# if $props_to_print is provided, prints only those columns. otherwise
+# takes all fields of the results property, with a fallback
+# to all fields occuring in items of $data.
 sub print_api_list {
-    my ($props_to_print, $data, $returninfo) = @_;
+    my ($data, $result_schema, $props_to_print) = @_;
 
-    die "can only print array result" if $returninfo->{type} ne 'array';
+    die "can only print object lists\n"
+	if !($result_schema->{type} eq 'array' && $result_schema->{items}->{type} eq 'object');
 
-    my $returnprops = $returninfo->{items}->{properties};
+    my $returnprops = $result_schema->{items}->{properties};
+
+    if (!defined($props_to_print)) {
+	$props_to_print = [ sort keys %$returnprops ];
+	if (!scalar(@$props_to_print)) {
+	    my $all_props = {};
+	    foreach my $obj (@{$data}) {
+		foreach my $key (keys %{$obj}) {
+		    $all_props->{ $key } = 1;
+		}
+	    }
+	    $props_to_print = [ sort keys %{$all_props} ];
+	}
+	die "unable to detect list properties\n" if !scalar(@$props_to_print);
+    }
 
     my $formatopts = [];
     foreach my $prop ( @$props_to_print ) {
@@ -512,6 +537,37 @@ sub print_api_list {
     }
 
     print_text_table($formatopts, $data);
+}
+
+sub print_api_result {
+    my ($format, $data, $result_schema) = @_;
+
+    return if $result_schema->{type} eq 'null';
+
+    if ($format eq 'json') {
+	print to_json($data, {utf8 => 1, allow_nonref => 1, canonical => 1, pretty => 1 });
+    } elsif ($format eq 'text') {
+	my $type = $result_schema->{type};
+	if ($type eq 'object') {
+	    foreach my $key (sort keys %$data) {
+		print $key . ": " .  data_to_text($data->{$key}) . "\n";
+	    }
+	} elsif ($type eq 'array') {
+	    return if !scalar(@$data);
+	    my $item_type = $result_schema->{items}->{type};
+	    if ($item_type eq 'object') {
+		print_api_list($data, $result_schema);
+	    } else {
+		foreach my $entry (@$data) {
+		    print data_to_text($entry) . "\n";
+		}
+	    }
+	} else {
+	    print "$data\n";
+	}
+    } else {
+	die "internal error: unknown output format"; # should not happen
+    }
 }
 
 sub verify_api {
@@ -616,8 +672,8 @@ my $handle_cmd  = sub {
     my $res = $class->cli_handler($cmd_str, $name, $cmd_args, $arg_param, $uri_param, $read_password_func, $param_mapping_func);
 
     if (defined $outsub) {
-	my $returninfo = $class->map_method_by_name($name)->{returns};
-	$outsub->($res, $returninfo);
+	my $result_schema = $class->map_method_by_name($name)->{returns};
+	$outsub->($res, $result_schema);
     }
 };
 
@@ -654,8 +710,8 @@ my $handle_simple_cmd = sub {
     my $res = $class->cli_handler($name, $name, \@ARGV, $arg_param, $uri_param, $read_password_func, $param_mapping_func);
 
     if (defined $outsub) {
-	my $returninfo = $class->map_method_by_name($name)->{returns};
-	$outsub->($res, $returninfo);
+	my $result_schema = $class->map_method_by_name($name)->{returns};
+	$outsub->($res, $result_schema);
     }
 };
 
